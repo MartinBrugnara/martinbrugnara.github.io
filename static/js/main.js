b3582187c37d1bb659c36e1c1d0f6001bad363d9
Martin Brugnara <martin.brugnara@gmail.com>
2013-04-10 17:22:17 +0200

    get a try

---
+++ static/js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $term = $('#terminal');
+    $.getJSON({
+        url: '/static/json/terminal.json',
+        success: success_handler,
+        error: error_handler
+    });
+
+    function success_handler(data){
+        user = data['user'];
+        domain = data['domain'];
+        $.each(data['rows'], function(row_data){
+            insert_row(user, domain, row_data);
+            $term.append("<li class='cmd'><span class='user'>"+user+"</span>" +
+            "<span class='at'></span><span class='domain'>"+domain+"</span>:" +
+            "<span class='path'>~</span><br>" +
+            "->&nbsp;<span class='command'>"+row_data['command']+"</span></li>"+
+            "<li class='output'>"+row_data['output']+"</li><br>");
+        });
+    };
+
+    function error_handler(e, j, c){
+        $term.append("<p class='command'>[001] Booting error. Please restart</p>");
+    };
+});
